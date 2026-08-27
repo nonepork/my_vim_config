900bf1b441974965dc892bc38a592c8ea1f03e19
nonepork <59335048+nonepork@users.noreply.github.com>
2026-08-27 22:11:16 +0800

    💩 Fix lazydev conf

--- neovim/lua/plugins/lsps.lua
+++ neovim/lua/plugins/lsps.lua
@@ -83,7 +83,7 @@ local servers = {
           checkThirdParty = false,
           -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
           --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-          library = vim.api.nvim_get_runtime_file('', true),
+          -- library = vim.api.nvim_get_runtime_file('', true),
         },
       })
     end,
