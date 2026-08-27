736acf64308e5e7d95183b9acae59b4112459f6d
nonepork <59335048+nonepork@users.noreply.github.com>
2026-08-27 22:12:11 +0800

    🐛 Fix lspconfig path for lua_ls

--- neovim/lua/plugins/lazydev.lua
+++ neovim/lua/plugins/lazydev.lua
@@ -4,10 +4,11 @@ vim.pack.add { gh 'folke/lazydev.nvim' }
 require('lazydev').setup {
   -- NOTE: idk if this is correctly configured or not.
   --
-  -- library = {
-  --   { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-  --   { path = '${3rd}/busted/library', words = { 'describe', 'it', 'before_each', 'after_each' } },
-  -- },
+  library = {
+    --   { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+    --   { path = '${3rd}/busted/library', words = { 'describe', 'it', 'before_each', 'after_each' } },
+    { path = 'nvim-lspconfig', words = { 'lspconfig' } },
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
